6843f6a1b0a0da28c22faf75ea3db2a19a1eb208
Zayed-Rahat <azurahat129@gmail.com>
2023-12-25 02:30:55 +0600

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,26 @@
+require("@nomiclabs/hardhat-truffle4");
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+
+//const PRIVATE_KEY = "YOUR SEPOLIA PRIVATE KEY";
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [PRIVATE_KEY]
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [PRIVATE_KEY]
+    },
+    alfajores: {
+      url: `https://celo-alfajores.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [PRIVATE_KEY]
+    }  
+  }
+};
